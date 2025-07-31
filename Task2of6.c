05f57db0d3a56b03c199c9d7ce88a6bd7a365195
Stas Kavitski <Stas.Kavitski@s.telhai.tech>
2025-07-31 21:33:48 +0300

    char

--- Task2of6.c
+++ Task2of6.c
@@ -2,6 +2,15 @@
 
 int main() 
 {
+	char ch = "";
+	printf("Enter char: ");
+	scanf_s("%c", &ch);
+//	printf("Your character is : %c", ch);
+	if (ch >= 48 && ch <= 57)
+	{
+		printf("Your character is a number: %c", ch);
+	}
+	
 
 	return 0;
 }
